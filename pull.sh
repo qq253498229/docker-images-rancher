d9edac0bc6456d1d1724825edf2513ca557b860e
王斌 <253498229@qq.com>
2019-03-15 23:31:29 +0800

    polish

--- pull.sh
+++ pull.sh
@@ -4,8 +4,9 @@
 #rancher/rancher         stable
 #rancher/rke-tools:v0.1.16
 #rancher/hyperkube:v1.13.4-rancher1
+#rancher/coreos-etcd:v3.2.24
 
-images=(rancher:stable rancher-agent:v2.1.7 rke-tools:v0.1.16 hyperkube:v1.13.4-rancher1)
+images=(rancher:stable rancher-agent:v2.1.7 rke-tools:v0.1.16 hyperkube:v1.13.4-rancher1 coreos-etcd:v3.2.24)
 
 for image in ${images[@]}; do
   docker pull registry.cn-beijing.aliyuncs.com/rancherc/$image
