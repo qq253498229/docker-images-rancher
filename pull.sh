4c29cc2d3d20bdcc54b838b3930e4ff5d4170d22
王斌 <253498229@qq.com>
2019-03-15 23:12:46 +0800

    initial commit

--- pull.sh
+++ pull.sh
@@ -7,7 +7,7 @@
 images=(rancher/rancher:stable rancher/rancher-agent:v2.1.7)
 
 for image in ${images[@]}; do
-  docker pull registry.cn-beijing.aliyuncs.com/wangdali/$image
-  docker tag registry.cn-beijing.aliyuncs.com/wangdali/$image rancher/$image
-  docker rmi registry.cn-beijing.aliyuncs.com/wangdali/$image
+  docker pull registry.cn-beijing.aliyuncs.com/rancherc/$image
+  docker tag registry.cn-beijing.aliyuncs.com/rancherc/$image rancher/$image
+  docker rmi registry.cn-beijing.aliyuncs.com/rancherc/$image
 done
